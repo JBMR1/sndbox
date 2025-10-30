90c9276418146350b86d318423ef481ff2cb9c8b
JBMR1 <205656754+JBMR1@users.noreply.github.com>
2025-10-29 20:26:41 -0400

    setting up mvc tier - simple entity DAO

--- sndbox-api/sndbox/src/main/java/com/desserabit/app/model/Entity.java
+++ sndbox-api/sndbox/src/main/java/com/desserabit/app/model/Entity.java
@@ -6,8 +6,27 @@ public class Entity {
     private String name;
 
 
-    public Entity{
+    public Entity(
         @JsonProperty("id") int id,
-        @JsonProperty("name") String name
+        @JsonProperty("name") String name){
+            this.id=id;
+            this.name=name;
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public String geString(){
+        return name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
+
